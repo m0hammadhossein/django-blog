e209f496bb8509ccfca894387edfa4e472d3dd06
m0hammadhossein <mohammadhossein.mail8731@gmail.com>
2022-02-04 22:42:44 +0330

    updated

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -117,7 +117,7 @@ USE_I18N = True
 USE_TZ = True
 
 AUTH_USER_MODEL = 'accounts.User'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
